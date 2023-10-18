251da09c225fb6c268ac0f5579a55d5dcd734f9d
phanquocsang97 <quocsang0310@gmail.com>
2023-10-18 14:13:01 +0700

    up

---
+++ exercise/ss2/exercise1/src/App.js
@@ -0,0 +1,58 @@
+import logo from './logo.svg';
+import './App.css';
+import React from "react";
+const students = [
+    {
+        company: 'Alfreds Futterkiste',
+        contact: 'Maria Anders',
+        country: 'Germany'
+    },
+    {
+        company: 'Centro comercial Moctezuma',
+        contact: 'Francisco Chang',
+        country: 'Mexico'
+    },
+    {
+        company: 'Ernst Handel',
+        contact: 'Roland Mendel',
+        country: 'Austria'
+    },
+    {
+        company: 'Island Trading',
+        contact: 'Helen Bennett',
+        country: 'UK'
+    },
+    {
+        company: 'Laughing Bacchus Winecellars',
+        contact: 'Yoshi Tannamuri',
+        country: 'Canada'
+    },
+    {
+        company: 'Magazzini Alimentari Riuniti',
+        contact: 'Giovanni Rovelli',
+        country: 'Italy'
+    }
+]
+function App() {
+  return (
+      <div className="container">
+          <h1>Students</h1>
+          <table>
+              <tr>
+                  <th>Company</th>
+                  <th>Contact</th>
+                  <th>Country</th>
+              </tr>
+              {students.map(student => (
+                  <tr>
+                      <td>{student.company}</td>
+                      <td>{student.contact}</td>
+                      <td>{student.country}</td>
+                  </tr>
+              ))}
+          </table>
+      </div>
+  );
+}
+
+export default App;
