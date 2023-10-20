a33ac6c2c6db1a6efec9b95fe65511e2e650ee0a
phanquocsang97 <quocsang0310@gmail.com>
2023-10-20 14:56:20 +0700

    up

--- exercise/ss4/exercise1/src/App.js
+++ exercise/ss4/exercise1/src/App.js
@@ -2,32 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import React from "react";
 import {useState} from "react";
+import Counter from "./componets/Counter";
 
 function App() {
-
-  const [counterFirst,setCounterFirst] = useState(1);
-  const [counterSecond,setCounterSecond] = useState(2);
-
-  const increaseOne = () => {
-    setCounterFirst(counterFirst + 1);
-  }
-
-  const increaseTwo = () => {
-    setCounterSecond(counterSecond + 2);
-  }
-  return (
-    <div className="App">
-        <h1>Count : {counterFirst}</h1>
-        <button onClick={increaseOne}>
-            Add 1
-        </button>
-        <br/>
-        <h1>Count : {counterSecond}</h1>
-        <button onClick={increaseTwo}>
-            Add 2
-        </button>
-    </div>
-  );
+    return (
+        <>
+            <Counter/>
+        </>
+    )
 }
 
 export default App;
---
+++ exercise/ss4/exercise1/src/componets/Counter.js
@@ -0,0 +1,30 @@
+import React, {useState} from "react";
+
+function Counter() {
+    const [counterFirst, setCounterFirst] = useState(1);
+    const [counterSecond, setCounterSecond] = useState(2);
+
+    const increaseOne = () => {
+        setCounterFirst(counterFirst + 1);
+    }
+
+    const increaseTwo = () => {
+        setCounterSecond(counterSecond + 2);
+    }
+
+    return (
+        <div className="App">
+            <h1>Count : {counterFirst}</h1>
+            <button onClick={increaseOne}>
+                Add 1
+            </button>
+            <br/>
+            <h1>Count : {counterSecond}</h1>
+            <button onClick={increaseTwo}>
+                Add 2
+            </button>
+        </div>
+    );
+}
+
+export default Counter;
